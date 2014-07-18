96beb916bd78ce5ce330c0b6176fd7bc1ea9d0b3
teymourff <teymourff@gmail.com>
2014-07-18 14:13:45 -0400

    changes

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require 'bundler' #require bundler
+require './lib/final.rb'
 Bundler.require #require everything in bundler in gemfile
-require './lib/emailer'
-require './lib/scraper'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+    name1 = params[:companysymbol1] #takes the value from the form named companysymbol1
+    name2 = params[:companysymbol2]
+    name3 = params[:companysymbol3]
+    name4 = params[:companysymbol4]
+    name5 = params[:companysymbol5]
+    name6 = params[:companysymbol6]
+    name7 = params[:companysymbol7]
+    name8 = params[:companysymbol8]
+    name9 = params[:companysymbol9]
+    name10 = params[:companysymbol10]
+    name11 = params[:companysymbol11]
+    name12 = params[:companysymbol12]
+    name13 = params[:companysymbol13]
+    name14 = params[:companysymbol14]
+    name15 = params[:companysymbol15]
+    names = [name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12, name13, name14, name15] 
+    @mystocks = Stocks.new(names) #feeds in name1 as an array to final.rb (new runs initialize)
+    @mystocks.scrape
+    @mystocks.diagnostic
+    erb :index #display original page again
+end
--- lib/final.rb
+++ lib/final.rb
@@ -1,14 +1,13 @@
 require 'open-uri'
-require 'nokogiri'
-require 'rubygems'
-require 'twilio-ruby'
+
 
 class Stocks
 
   attr_accessor :companynames, :dv, :percentage, :pcp, :webpagefiles
 
-  def initialize
-    @companynames = ["AAPL", "GOOG"]
+  def initialize(companynames)
+    @companynames = companynames
+    @companynames.reject! { |c| c.empty? } #looks for empty elements in the array and deletes them
     @usernumber = 19177542295
     @substantialdrop = 0.00000000000000000000002
     @updateinterval = 1 * 10
@@ -32,20 +31,25 @@ class Stocks
 
   def scrape
     repeat = 0
-
     while repeat < @companynames.size
       html = open(@stockpages[repeat]) #this will go to the page of the users stock to scrape the data
       @webpagefiles[repeat] = Nokogiri::HTML(html)
-      #dollar value:
-      @dv[repeat] = @webpagefiles[repeat].css("div#content .quotebar-wrap .header-wrap .quote-wrap .quote-data-wrap #qwidget-quote-wrap #qwidget_quote #qwidget_lastsale").children.last.text.to_f
-      #percentage up or down:
-      @percentage[repeat] = @webpagefiles[repeat].css("div#content .quotebar-wrap .header-wrap .quote-wrap .quote-data-wrap #qwidget-quote-wrap #qwidget_quote #qwidget_percent").children.last.text.to_f
-      #previous closing price:
-      @pcp[repeat] = @webpagefiles[repeat].css("#quotes_content_left__PreviousClose").children.last.text.to_f
-      if Time.now - @lastupdate >= @updateinterval #checks if its been an hour fdv was updated
-        @fdv = @dv
-        @lastupdate = Time.now
+      if @webpagefiles[repeat].css("div#content .quotebar-wrap .header-wrap .quote-wrap .quote-data-wrap #qwidget-quote-wrap #qwidget_quote #qwidget_lastsale").children.last.nil?
+        @companynames.delete_at(repeat) # if the page is invalid, the item is removed from the string
+        repeat -= 1 #when you delete the element in the array, everything goes back so you have to go back one too
+      else
+        #dollar value:
+        @dv[repeat] = @webpagefiles[repeat].css("div#content .quotebar-wrap .header-wrap .quote-wrap .quote-data-wrap #qwidget-quote-wrap #qwidget_quote #qwidget_lastsale").children.last.text.gsub("$", "").to_f
+        #percentage up or down:
+        @percentage[repeat] = @webpagefiles[repeat].css("div#content .quotebar-wrap .header-wrap .quote-wrap .quote-data-wrap #qwidget-quote-wrap #qwidget_quote #qwidget_percent").children.last.text.to_f
+        #previous closing price:
+        @pcp[repeat] = @webpagefiles[repeat].css("#quotes_content_left__PreviousClose").children.last.text.to_f
+        if Time.now - @lastupdate >= @updateinterval #checks if its been an hour fdv was updated
+          @fdv = @dv
+          @lastupdate = Time.now
+        end
       end
+
       repeat += 1
     end #while loop end
   end #scrape
@@ -84,15 +88,33 @@ class Stocks
 
         @client.account.messages.create(
           :from => '+18152642023',
-          :to => usernumber,
+          :to => @usernumber,
           :body => textmessage
         ) 
 
   end #text if down end
 
+  def diagnostic
+      account_sid = 'ACe330ba04d082392df4cb3511dcb72cec'
+      auth_token = '2008ea097713e401a16c54029058da82'
+
+      # set up a client to talk to the Twilio REST API
+      @client = Twilio::REST::Client.new account_sid, auth_token
+
+      companynames.each_index do |i|
+        textmessage = @companynames[i] + " dv:" + @dv[i].to_s + " percentage:" + @percentage[i].to_s + " Pcp:" + @pcp[i].to_s
+
+        @client.account.messages.create(
+          :from => '+18152642023',
+          :to => @usernumber,
+          :body => textmessage
+          )
+      end
+  end
+
 end #class end
 
-mystocks = Stocks.new
+# mystocks = Stocks.new
 
 
 
